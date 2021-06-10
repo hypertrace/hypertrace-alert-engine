2dbe8594365409f8528a8b61f25151328e12bdb9
rish691 <rishabh@traceable.ai>
2021-06-10 10:17:25 +0530

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("org.hypertrace.repository-plugin") version "0.4.0"
+    id("org.hypertrace.ci-utils-plugin") version "0.3.0"
+    id("org.hypertrace.jacoco-report-plugin") version "0.2.0" apply false
+    id("org.hypertrace.avro-plugin") version "0.3.1" apply false
+    id("org.hypertrace.code-style-plugin") version "1.1.0" apply false
+}
+
+subprojects {
+    group = "org.hypertrace.alerting"
+
+    pluginManager.withPlugin("org.hypertrace.publish-plugin") {
+        configure<org.hypertrace.gradle.publishing.HypertracePublishExtension> {
+            license.set(org.hypertrace.gradle.publishing.License.TRACEABLE_COMMUNITY)
+        }
+    }
+
+    pluginManager.withPlugin("java") {
+        configure<JavaPluginExtension> {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+
+            apply(plugin = "org.hypertrace.code-style-plugin")
+        }
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        maven("https://hypertrace.jfrog.io/artifactory/maven")
+    }
+}
+
+plugins {
+    id("org.hypertrace.version-settings") version "0.2.0"
+}
+
+rootProject.name = "alerting"
+
