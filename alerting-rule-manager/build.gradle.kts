f8b1030caf1af89ae5b2b7acd813fabb172e3835
rish691 <rishabh@traceable.ai>
2021-06-16 17:22:06 +0530

    address some comments

--- alerting-rule-manager/build.gradle.kts
+++ alerting-rule-manager/build.gradle.kts
@@ -11,17 +11,8 @@ application {
 }
 
 dependencies {
-    implementation("org.hypertrace.core.datamodel:data-model:0.1.15")
     implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.23")
     implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.23")
-
-    // Required for the GRPC clients.
-    runtimeOnly("io.grpc:grpc-netty:1.36.1")
-
-    implementation("com.typesafe:config:1.4.1")
-    implementation("io.confluent:kafka-avro-serializer:6.0.1")
-    implementation("org.apache.commons:commons-lang3:3.12.0")
-    implementation("org.apache.httpcomponents:httpclient:4.5.13")
 
     // Logging
     implementation("org.slf4j:slf4j-api:1.7.30")
--- metric-anomaly-detector/build.gradle.kts
+++ metric-anomaly-detector/build.gradle.kts
@@ -14,14 +14,6 @@ dependencies {
     implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.23")
     implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.23")
 
-    // Required for the GRPC clients.
-    runtimeOnly("io.grpc:grpc-netty:1.36.1")
-
-    implementation("com.typesafe:config:1.4.1")
-    implementation("io.confluent:kafka-avro-serializer:6.0.1")
-    implementation("org.apache.commons:commons-lang3:3.12.0")
-    implementation("org.apache.httpcomponents:httpclient:4.5.13")
-
     // Logging
     implementation("org.slf4j:slf4j-api:1.7.30")
     runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
