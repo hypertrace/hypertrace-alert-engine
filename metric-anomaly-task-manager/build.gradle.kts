225d7c67f39d3964698fa70a9ad55027943a0af1
Ronak <ronak@traceable.ai>
2021-06-18 18:34:34 +0530

    feat : read json to alerting proto

--- metric-anomaly-task-manager/build.gradle.kts
+++ metric-anomaly-task-manager/build.gradle.kts
@@ -11,10 +11,22 @@ application {
 }
 
 dependencies {
+    implementation(project(":metric-anomaly-data-model"))
+
     implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.23")
     implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.23")
+
+    // protobuf
+    implementation("com.google.protobuf:protobuf-java-util:4.0.0-rc-2")
 
     // Logging
     implementation("org.slf4j:slf4j-api:1.7.30")
     runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
+
+    // testing
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
