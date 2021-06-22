3d0d5fc26cb287fc02941c3ddc041cdf53e1ef8e
Ronak <ronak@traceable.ai>
2021-06-22 15:48:42 +0530

    wip

--- metric-anomaly-task-manager/src/main/java/org/hypertrace/alert/engine/metric/anomaly/task/manager/MetricAnomalyTaskManager.java
+++ metric-anomaly-task-manager/src/main/java/org/hypertrace/alert/engine/metric/anomaly/task/manager/MetricAnomalyTaskManager.java
@@ -1,11 +1,29 @@
 package org.hypertrace.alert.engine.metric.anomaly.task.manager;
 
+import com.typesafe.config.Config;
+import org.hypertrace.alert.engine.metric.anomaly.task.manager.common.DataSource;
+import org.hypertrace.alert.engine.metric.anomaly.task.manager.common.DataSourceProvider;
 import org.hypertrace.core.serviceframework.PlatformService;
+import org.hypertrace.core.serviceframework.config.ConfigClient;
 
 public class MetricAnomalyTaskManager extends PlatformService {
+  private static final String SERVICE_NAME_CONFIG = "service.name";
+  private static final String SERVICE_PORT_CONFIG = "service.port";
+  private static final String DATA_SOURCE_CONFIG = "dataSource";
+
+  private DataSource dataSource;
+
+  public MetricAnomalyTaskManager(ConfigClient configClient) {
+    super(configClient);
+  }
 
   @Override
-  protected void doInit() {}
+  protected void doInit() {
+    Config dataSourceConfig = getAppConfig().getConfig(DATA_SOURCE_CONFIG);
+    this.dataSource =
+        DataSourceProvider.getDataSource(dataSourceConfig);
+
+  }
 
   @Override
   protected void doStart() {}
---
+++ metric-anomaly-task-manager/src/main/java/org/hypertrace/alert/engine/metric/anomaly/task/manager/common/FileDataSource.java
@@ -0,0 +1,11 @@
+package org.hypertrace.alert.engine.metric.anomaly.task.manager.common;
+
+import com.typesafe.config.Config;
+
+public class FileDataSource implements DataSource {
+
+  public void init(Config config) {
+
+  }
+
+}
