702a84b029c11a229b57006f673fbd9ebe79a946
Ronak <ronak@traceable.ai>
2021-06-23 13:17:32 +0530

    address comments

--- metric-anomaly-task-manager/build.gradle.kts
+++ metric-anomaly-task-manager/build.gradle.kts
@@ -11,6 +11,7 @@ application {
 }
 
 dependencies {
+    implementation(project(":metric-anomaly-data-model"))
     implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.23")
     implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.23")
 
