a3b11383af54a43644230acd3d2bc5d086c70549
Ronak <ronak@traceable.ai>
2021-06-30 15:12:30 +0530

    feat: adds alert task avdl message and kafka consumer, producer

--- metric-anomaly-task-manager/src/main/java/org/hypertrace/alert/engine/metric/anomaly/task/manager/MetricAnomalyTaskManager.java
+++ metric-anomaly-task-manager/src/main/java/org/hypertrace/alert/engine/metric/anomaly/task/manager/MetricAnomalyTaskManager.java
@@ -1,56 +1,20 @@
 package org.hypertrace.alert.engine.metric.anomaly.task.manager;
 
-import org.hypertrace.alert.engine.metric.anomaly.task.manager.job.AlertTaskJobManager;
-import org.hypertrace.alert.engine.metric.anomaly.task.manager.job.JobManager;
 import org.hypertrace.core.serviceframework.PlatformService;
-import org.hypertrace.core.serviceframework.config.ConfigClient;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.SchedulerFactory;
-import org.quartz.impl.StdSchedulerFactory;
 
 public class MetricAnomalyTaskManager extends PlatformService {
-  private Scheduler scheduler;
-  private JobManager jobManager;
-
-  public MetricAnomalyTaskManager(ConfigClient configClient) {
-    super(configClient);
-  }
 
   @Override
-  protected void doInit() {
-    try {
-      SchedulerFactory schedulerFactory = new StdSchedulerFactory();
-      scheduler = schedulerFactory.getScheduler();
-      jobManager = new AlertTaskJobManager();
-      jobManager.initJob(getAppConfig());
-    } catch (SchedulerException e) {
-      throw new RuntimeException(e);
-    }
-  }
+  protected void doInit() {}
 
   @Override
-  protected void doStart() {
-    try {
-      jobManager.startJob(scheduler);
-      scheduler.start();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
+  protected void doStart() {}
 
   @Override
-  protected void doStop() {
-    try {
-      jobManager.stopJob(scheduler);
-      scheduler.shutdown();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
+  protected void doStop() {}
 
   @Override
   public boolean healthCheck() {
-    return true;
+    return false;
   }
 }
