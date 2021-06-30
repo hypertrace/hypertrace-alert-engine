5f0ad561e09804e61e3e093e599b6b5760f3b648
Ronak <ronak@traceable.ai>
2021-06-30 22:26:30 +0530

    feat:adds producer component for alerting

--- metric-anomaly-task-manager/src/main/java/org/hypertrace/alert/engine/metric/anomaly/task/manager/MetricAnomalyTaskManager.java
+++ metric-anomaly-task-manager/src/main/java/org/hypertrace/alert/engine/metric/anomaly/task/manager/MetricAnomalyTaskManager.java
@@ -1,20 +1,56 @@
 package org.hypertrace.alert.engine.metric.anomaly.task.manager;
 
+import org.hypertrace.alert.engine.metric.anomaly.task.manager.job.AlertTaskJobManager;
+import org.hypertrace.alert.engine.metric.anomaly.task.manager.job.JobManager;
 import org.hypertrace.core.serviceframework.PlatformService;
+import org.hypertrace.core.serviceframework.config.ConfigClient;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.impl.StdSchedulerFactory;
 
 public class MetricAnomalyTaskManager extends PlatformService {
+  private Scheduler scheduler;
+  private JobManager jobManager;
+
+  public MetricAnomalyTaskManager(ConfigClient configClient) {
+    super(configClient);
+  }
 
   @Override
-  protected void doInit() {}
+  protected void doInit() {
+    try {
+      SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+      scheduler = schedulerFactory.getScheduler();
+      jobManager = new AlertTaskJobManager();
+      jobManager.initJob(getAppConfig());
+    } catch (SchedulerException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   @Override
-  protected void doStart() {}
+  protected void doStart() {
+    try {
+      jobManager.startJob(scheduler);
+      scheduler.start();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   @Override
-  protected void doStop() {}
+  protected void doStop() {
+    try {
+      jobManager.stopJob(scheduler);
+      scheduler.shutdown();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   @Override
   public boolean healthCheck() {
-    return false;
+    return true;
   }
 }
---
+++ metric-anomaly-task-manager/src/main/java/org/hypertrace/alert/engine/metric/anomaly/task/manager/rulesource/FSRuleSource.java
@@ -0,0 +1,41 @@
+package org.hypertrace.alert.engine.metric.anomaly.task.manager.rulesource;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.typesafe.config.Config;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import org.hypertrace.core.documentstore.Document;
+import org.hypertrace.core.documentstore.JSONDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FSRuleSource implements RuleSource {
+  private static final Logger LOGGER = LoggerFactory.getLogger(FSRuleSource.class);
+  private static final String PATH_CONFIG = "path";
+
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+  private Config fsConfig;
+
+  public FSRuleSource(Config fsConfig) {
+    this.fsConfig = fsConfig;
+  }
+
+  public List<Document> getAllEventConditions(String type) throws IOException {
+    String fsPath = fsConfig.getString(PATH_CONFIG);
+    LOGGER.info("Reading rules rules from file path:{}", fsPath);
+
+    JsonNode jsonNode = OBJECT_MAPPER.readTree(new File(fsPath));
+    if (!jsonNode.isArray()) {
+      throw new IOException("File should contain an array of notification rules");
+    }
+
+    return StreamSupport.stream(jsonNode.spliterator(), false)
+        .map(node -> new JSONDocument(node))
+        .collect(Collectors.toList());
+  }
+}
