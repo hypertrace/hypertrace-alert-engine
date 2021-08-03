35df7b4f6fb1de7edc9f57a58095f27fae5a618a
rish691 <rishabh@traceable.ai>
2021-08-03 13:40:01 +0530

    add dependencies for integ test

--- hypertrace-alert-engine/build.gradle.kts
+++ hypertrace-alert-engine/build.gradle.kts
@@ -12,10 +12,6 @@ application {
     mainClass.set("org.hypertrace.core.serviceframework.PlatformServiceLauncher")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
 dependencies {
     constraints {
         implementation("com.fasterxml.jackson.core:jackson-databind:2.12.1")
@@ -38,8 +34,36 @@ dependencies {
     // framework + libs
     implementation("com.google.protobuf:protobuf-java-util:4.0.0-rc-2")
     implementation("org.quartz-scheduler:quartz:2.3.2")
+
+    // integration test
+    integrationTestImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    integrationTestImplementation("org.junit.jupiter:junit-jupiter-params:5.7.1")
+    integrationTestImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    integrationTestImplementation("org.testcontainers:testcontainers:1.15.2")
+    integrationTestImplementation("org.testcontainers:junit-jupiter:1.15.2")
+    integrationTestImplementation("org.testcontainers:kafka:1.15.2")
+    integrationTestImplementation("org.hypertrace.core.serviceframework:integrationtest-service-framework:0.1.23")
+    integrationTestImplementation("com.github.stefanbirkner:system-lambda:1.2.0")
+
+    integrationTestImplementation("org.apache.kafka:kafka-clients:5.5.1-ccs")
+    integrationTestImplementation("org.apache.kafka:kafka-streams:5.5.1-ccs")
+    integrationTestImplementation("org.apache.avro:avro:1.10.2")
+    integrationTestImplementation("com.google.guava:guava:30.1.1-jre")
+    integrationTestImplementation("org.hypertrace.core.datamodel:data-model:0.1.12")
+    integrationTestImplementation("org.hypertrace.core.kafkastreams.framework:kafka-streams-serdes:0.1.13")
+    integrationTestImplementation("org.hypertrace.core.query.service:query-service-client:0.5.2")
+    integrationTestImplementation("org.hypertrace.core.attribute.service:attribute-service-client:0.12.0")
+    integrationTestImplementation("com.squareup.okhttp3:mockwebserver:4.9.1")
 }
 
 tasks.run<JavaExec> {
     jvmArgs = listOf("-Dservice.name=${project.name}")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.integrationTest {
+    useJUnitPlatform()
 }
