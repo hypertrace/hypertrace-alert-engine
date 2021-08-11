a5854e45596e7dd1c8fee75fde6d7f87e1c37e4a
rish691 <rishabh@traceable.ai>
2021-08-11 20:18:51 +0530

    Move notification event building logic to StaticRuleEvaluator

--- metric-anomaly-detector/src/main/java/org/hypertrace/alert/engine/metric/anomaly/detector/evaluator/StaticRuleEvaluator.java
+++ metric-anomaly-detector/src/main/java/org/hypertrace/alert/engine/metric/anomaly/detector/evaluator/StaticRuleEvaluator.java
@@ -2,14 +2,21 @@ package org.hypertrace.alert.engine.metric.anomaly.detector.evaluator;
 
 import static org.hypertrace.alert.engine.metric.anomaly.detector.MetricAnomalyDetectorConstants.TENANT_ID_KEY;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import org.hypertrace.alert.engine.eventcondition.config.service.v1.MetricAnomalyEventCondition;
 import org.hypertrace.alert.engine.eventcondition.config.service.v1.StaticThresholdCondition;
 import org.hypertrace.alert.engine.metric.anomaly.datamodel.AlertTask;
-import org.hypertrace.alert.engine.metric.anomaly.datamodel.Operator;
+import org.hypertrace.alert.engine.metric.anomaly.datamodel.EventRecord;
+import org.hypertrace.alert.engine.metric.anomaly.datamodel.MetricAnomalyNotificationEvent;
+import org.hypertrace.alert.engine.metric.anomaly.datamodel.NotificationEvent;
+import org.hypertrace.alert.engine.metric.anomaly.datamodel.StaticRuleViolationSummary;
+import org.hypertrace.alert.engine.metric.anomaly.datamodel.StaticThresholdOperator;
+import org.hypertrace.alert.engine.metric.anomaly.datamodel.ViolationSummary;
 import org.hypertrace.core.query.service.api.QueryRequest;
 import org.hypertrace.core.query.service.api.ResultSetChunk;
 import org.hypertrace.core.query.service.api.Row;
@@ -28,11 +35,16 @@ public class StaticRuleEvaluator {
     this.queryRequestHandler = queryRequestHandler;
   }
 
-  EvaluationResult evaluateRule(
-      MetricAnomalyEventCondition metricAnomalyEventCondition, AlertTask alertTask) {
+  Optional<NotificationEvent> evaluateRule(
+      MetricAnomalyEventCondition metricAnomalyEventCondition, AlertTask alertTask)
+      throws IOException {
 
     QueryRequest queryRequest =
-        queryRequestHandler.getQueryRequest(metricAnomalyEventCondition, alertTask);
+        queryRequestHandler.getQueryRequest(
+            metricAnomalyEventCondition,
+            alertTask.getTenantId(),
+            alertTask.getLastExecutionTime(),
+            alertTask.getCurrentExecutionTime());
     LOGGER.debug("Query request {}", queryRequest);
 
     Iterator<ResultSetChunk> iterator =
@@ -71,15 +83,15 @@ public class StaticRuleEvaluator {
       LOGGER.debug("Rule normal. dataCount {} violationCount {}", dataCount, violationCount);
     }
 
-    return EvaluationResult.builder()
-        .dataCount(dataCount)
-        .violationCount(violationCount)
-        .isViolation(isViolation(dataCount, violationCount))
-        .operator(
-            staticThresholdOperatorToOperatorConvertor(staticThresholdCondition.getOperator()))
-        .rhs(getConditionRhs(staticThresholdCondition))
-        .metricValues(metricValues)
-        .build();
+    if (!isViolation(dataCount, violationCount)) {
+      return Optional.empty();
+    }
+
+    return getNotificationEvent(
+        alertTask, dataCount, violationCount,
+        staticThresholdOperatorToOperatorConvertor(staticThresholdCondition.getOperator()),
+        metricValues,
+        getConditionRhs(staticThresholdCondition));
   }
 
   boolean compareThreshold(Value value, StaticThresholdCondition staticThresholdCondition) {
@@ -92,7 +104,7 @@ public class StaticRuleEvaluator {
     return staticThresholdCondition.getValue();
   }
 
-  private boolean isViolation(int dataCount, int violationCount) {
+  static boolean isViolation(int dataCount, int violationCount) {
     return dataCount > 0 && (dataCount == violationCount);
   }
 
@@ -115,21 +127,71 @@ public class StaticRuleEvaluator {
     }
   }
 
-  private Operator staticThresholdOperatorToOperatorConvertor(
-      org.hypertrace.alert.engine.eventcondition.config.service.v1.StaticThresholdOperator
-          operator) {
+  private static StaticThresholdOperator staticThresholdOperatorToOperatorConvertor(
+      org.hypertrace.alert.engine.eventcondition.config.service.v1.StaticThresholdOperator operator) {
     switch (operator) {
       case STATIC_THRESHOLD_OPERATOR_GT:
-        return Operator.STATIC_THRESHOLD_OPERATOR_GT;
+        return StaticThresholdOperator.STATIC_THRESHOLD_OPERATOR_GT;
       case STATIC_THRESHOLD_OPERATOR_LT:
-        return Operator.STATIC_THRESHOLD_OPERATOR_LT;
+        return StaticThresholdOperator.STATIC_THRESHOLD_OPERATOR_LT;
       case STATIC_THRESHOLD_OPERATOR_GTE:
-        return Operator.STATIC_THRESHOLD_OPERATOR_GTE;
+        return StaticThresholdOperator.STATIC_THRESHOLD_OPERATOR_GTE;
       case STATIC_THRESHOLD_OPERATOR_LTE:
-        return Operator.STATIC_THRESHOLD_OPERATOR_LTE;
+        return StaticThresholdOperator.STATIC_THRESHOLD_OPERATOR_LTE;
       default:
         throw new UnsupportedOperationException(
             "Unsupported threshold condition operator: " + operator);
     }
+  }
+
+  private Optional<NotificationEvent> getNotificationEvent(
+      AlertTask alertTask,
+      int dataCount,
+      int violationCount,
+      StaticThresholdOperator operator,
+      List<Double> metricValues,
+      double staticThreshold)
+      throws IOException {
+
+    List<ViolationSummary> violationSummaryList = new ArrayList<>();
+
+    violationSummaryList.add(
+        ViolationSummary.newBuilder().setViolationSummary(
+            StaticRuleViolationSummary.newBuilder()
+                .setMetricValues(new ArrayList<>(metricValues))
+                .setStaticThreshold(staticThreshold)
+                .setDataCount(dataCount)
+                .setViolationCount(violationCount)
+                .setOperator(operator)
+                .build()
+        ).build()
+    );
+
+    MetricAnomalyNotificationEvent metricAnomalyNotificationEvent =
+        MetricAnomalyNotificationEvent.newBuilder()
+            .setViolationTimestamp(alertTask.getCurrentExecutionTime())
+            .setChannelId(alertTask.getChannelId())
+            .setEventConditionId(alertTask.getEventConditionId())
+            .setEventConditionType(alertTask.getEventConditionType())
+            .setViolationSummaryList(violationSummaryList)
+            .build();
+
+    EventRecord eventRecord =
+        EventRecord.newBuilder()
+            .setEventType(AlertRuleEvaluator.METRIC_ANOMALY_ACTION_EVENT_TYPE)
+            .setEventRecordMetadata(Map.of())
+            .setEventValue(metricAnomalyNotificationEvent.toByteBuffer())
+            .build();
+
+    NotificationEvent notificationEvent =
+        NotificationEvent.newBuilder()
+            .setTenantId(alertTask.getTenantId())
+            .setNotificationEventMetadata(Map.of())
+            .setEventTimeMillis(alertTask.getCurrentExecutionTime())
+            .setEventRecord(eventRecord)
+            .build();
+
+    LOGGER.debug("Notification Event {}", notificationEvent);
+    return Optional.of(notificationEvent);
   }
 }
