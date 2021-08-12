e57e66701d4645d8f2dbb0c113dcdd6e3214e010
rish691 <rishabh@traceable.ai>
2021-08-12 17:18:10 +0530

    Fix test

--- metric-anomaly-detector/src/test/java/org/hypertrace/alert/engine/metric/anomaly/detector/evaluator/AlertRuleEvaluatorTest.java
+++ metric-anomaly-detector/src/test/java/org/hypertrace/alert/engine/metric/anomaly/detector/evaluator/AlertRuleEvaluatorTest.java
@@ -182,7 +182,7 @@ public class AlertRuleEvaluatorTest {
 
     QueryRequest expectedQueryRequest =
         getExpectedQuery(
-            alertTaskBuilder.getLastExecutionTime(), alertTaskBuilder.getCurrentExecutionTime());
+            windowStartTime, alertTaskBuilder.getCurrentExecutionTime());
 
     when(queryServiceClient.executeQuery(
             eq(expectedQueryRequest), eq(Map.of("x-tenant-id", "__default")), eq(10000)))
