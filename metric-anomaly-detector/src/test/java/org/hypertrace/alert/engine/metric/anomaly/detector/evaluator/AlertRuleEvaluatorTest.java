c712f94730e59c67e767179e4bc196f17598a64d
rish691 <rishabh@traceable.ai>
2021-08-12 17:10:47 +0530

    Add unit test for Baseline rule

--- metric-anomaly-detector/src/test/java/org/hypertrace/alert/engine/metric/anomaly/detector/evaluator/AlertRuleEvaluatorTest.java
+++ metric-anomaly-detector/src/test/java/org/hypertrace/alert/engine/metric/anomaly/detector/evaluator/AlertRuleEvaluatorTest.java
@@ -2,12 +2,14 @@ package org.hypertrace.alert.engine.metric.anomaly.detector.evaluator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.refEq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.time.Duration;
 import java.util.Iterator;
@@ -17,6 +19,7 @@ import java.util.Optional;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
 import org.hypertrace.alert.engine.eventcondition.config.service.v1.Attribute;
+import org.hypertrace.alert.engine.eventcondition.config.service.v1.DynamicThresholdCondition;
 import org.hypertrace.alert.engine.eventcondition.config.service.v1.Filter;
 import org.hypertrace.alert.engine.eventcondition.config.service.v1.LeafFilter;
 import org.hypertrace.alert.engine.eventcondition.config.service.v1.LhsExpression;
@@ -51,56 +54,18 @@ import org.hypertrace.core.query.service.api.ValueType;
 import org.hypertrace.core.query.service.client.QueryServiceClient;
 import org.hypertrace.gateway.service.v1.common.FunctionType;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.stubbing.Answer;
 
 public class AlertRuleEvaluatorTest {
 
-  @Test
-  void testMetricAnomaly() throws URISyntaxException, IOException {
+  private static QueryServiceClient queryServiceClient;
+  private static AlertRuleEvaluator alertRuleEvaluator;
 
-    // create mock alertTask
-    LhsExpression lhsExpression = createLhsExpression("name", AttributeScope.SERVICE.name());
-    RhsExpression rhsExpression = createRhsExpression("customer");
-    LeafFilter leafFilter =
-        createLeafFilter(ValueOperator.VALUE_OPERATOR_EQ, lhsExpression, rhsExpression);
-
-    MetricSelection metricSelection =
-        MetricSelection.newBuilder()
-            .setMetricAggregationInterval("PT15s")
-            .setMetricAggregationFunction(
-                MetricAggregationFunction.METRIC_AGGREGATION_FUNCTION_TYPE_SUM)
-            .setFilter(Filter.newBuilder().setLeafFilter(leafFilter).build())
-            .setMetricAttribute(
-                Attribute.newBuilder().setKey("duration").setScope("SERVICE").build())
-            .build();
-
-    MetricAnomalyEventCondition.Builder metricAnomalyEventConditionBuilder =
-        MetricAnomalyEventCondition.newBuilder();
-    metricAnomalyEventConditionBuilder.setMetricSelection(metricSelection);
-    metricAnomalyEventConditionBuilder.addViolationCondition(
-        ViolationCondition.newBuilder()
-            .setStaticThresholdCondition(
-                StaticThresholdCondition.newBuilder()
-                    .setOperator(StaticThresholdOperator.STATIC_THRESHOLD_OPERATOR_GT)
-                    .setMinimumViolationDuration("PT5M")
-                    .setValue(15)
-                    .setSeverity(Severity.SEVERITY_CRITICAL)
-                    .build())
-            .build());
-
-    AlertTask.Builder alertTaskBuilder = AlertTask.newBuilder();
-    long timeStamp = System.currentTimeMillis();
-    alertTaskBuilder.setCurrentExecutionTime(timeStamp);
-    alertTaskBuilder.setLastExecutionTime(timeStamp - Duration.ofMinutes(1).toMillis());
-    alertTaskBuilder.setEventConditionId("event-condition-1");
-    alertTaskBuilder.setEventConditionType("MetricAnomalyEventCondition");
-    alertTaskBuilder.setTenantId("__default");
-    alertTaskBuilder.setEventConditionValue(
-        metricAnomalyEventConditionBuilder.build().toByteString().asReadOnlyByteBuffer());
-    alertTaskBuilder.setChannelId("channel1");
-
+  @BeforeAll
+  static void setup() throws URISyntaxException, MalformedURLException {
     // create mock config
     Config config =
         ConfigFactory.parseURL(
@@ -131,13 +96,182 @@ public class AlertRuleEvaluatorTest {
 
     AttributeServiceClient attributesServiceClient = mock(AttributeServiceClient.class);
     when(attributesServiceClient.findAttributes(
-            eq(Map.of("x-tenant-id", "__default")),
-            eq(
-                AttributeMetadataFilter.newBuilder()
-                    .addScopeString(AttributeScope.SERVICE.name())
-                    .build())))
+        eq(Map.of("x-tenant-id", "__default")),
+        eq(
+            AttributeMetadataFilter.newBuilder()
+                .addScopeString(AttributeScope.SERVICE.name())
+                .build())))
         .thenAnswer((Answer<Iterator<AttributeMetadata>>) invocation -> attributesList1.iterator());
 
+    // create mock queryServiceClient
+    queryServiceClient = Mockito.mock(QueryServiceClient.class);
+
+    alertRuleEvaluator =
+        new AlertRuleEvaluator(config, attributesServiceClient, queryServiceClient);
+  }
+
+  @Test
+  void testMetricAnomalyForStaticThreshold() throws IOException {
+    ViolationCondition violationCondition = ViolationCondition.newBuilder()
+        .setStaticThresholdCondition(
+            StaticThresholdCondition.newBuilder()
+                .setOperator(StaticThresholdOperator.STATIC_THRESHOLD_OPERATOR_GT)
+                .setMinimumViolationDuration("PT5M")
+                .setValue(15)
+                .setSeverity(Severity.SEVERITY_CRITICAL)
+                .build())
+        .build();
+
+    long timeStamp = System.currentTimeMillis();
+    AlertTask.Builder alertTaskBuilder = getTestAlertTask(violationCondition, timeStamp);
+
+    QueryRequest expectedQueryRequest = getExpectedQuery(
+        alertTaskBuilder.getLastExecutionTime(),
+        alertTaskBuilder.getCurrentExecutionTime());
+
+    when(queryServiceClient.executeQuery(
+        eq(expectedQueryRequest), eq(Map.of("x-tenant-id", "__default")), eq(10000)))
+        .thenReturn(
+            List.of(
+                getResultSetChunk(
+                    List.of(
+                        new StringJoiner(".")
+                            .add(AttributeScope.SERVICE.name())
+                            .add("startTime")
+                            .toString(),
+                        new StringJoiner(".")
+                            .add(AttributeScope.SERVICE.name())
+                            .add("duration")
+                            .toString()),
+                    new String[][]{
+                        { "60", "300" },
+                        { "120", "400" }
+                    }))
+                .iterator());
+
+    Optional<NotificationEvent> notificationEventOptional =
+        alertRuleEvaluator.process(alertTaskBuilder.build());
+    Assertions.assertTrue(notificationEventOptional.isPresent());
+
+    NotificationEvent notificationEvent = notificationEventOptional.get();
+    MetricAnomalyNotificationEvent metricAnomalyNotificationEvent =
+        MetricAnomalyNotificationEvent.fromByteBuffer(
+            notificationEvent.getEventRecord().getEventValue());
+
+    assertEquals("channel1", metricAnomalyNotificationEvent.getChannelId());
+    assertEquals("event-condition-1", metricAnomalyNotificationEvent.getEventConditionId());
+    assertEquals(
+        "MetricAnomalyEventCondition", metricAnomalyNotificationEvent.getEventConditionType());
+    assertEquals(timeStamp, metricAnomalyNotificationEvent.getViolationTimestamp());
+  }
+
+  @Test
+  void testMetricAnomalyForDynamicThreshold() throws IOException {
+    ViolationCondition violationCondition = ViolationCondition.newBuilder()
+        .setDynamicThresholdCondition(
+            DynamicThresholdCondition.newBuilder()
+                .setBaselineDuration("PT5M")
+                .build())
+        .build();
+
+    long timeStamp = System.currentTimeMillis();
+
+    AlertTask.Builder alertTaskBuilder = getTestAlertTask(violationCondition, timeStamp);
+
+    long windowStartTime =
+        alertTaskBuilder.getCurrentExecutionTime() - Duration.ofMinutes(5).toMillis();
+
+    QueryRequest expectedQueryRequest = getExpectedQuery(
+        alertTaskBuilder.getLastExecutionTime(),
+        alertTaskBuilder.getCurrentExecutionTime());
+
+    when(queryServiceClient.executeQuery(
+        eq(expectedQueryRequest), eq(Map.of("x-tenant-id", "__default")), eq(10000)))
+        .thenReturn(
+            List.of(
+                getResultSetChunk(
+                    List.of(
+                        new StringJoiner(".")
+                            .add(AttributeScope.SERVICE.name())
+                            .add("startTime")
+                            .toString(),
+                        new StringJoiner(".")
+                            .add(AttributeScope.SERVICE.name())
+                            .add("duration")
+                            .toString()),
+                    new String[][]{
+                        {String.valueOf(windowStartTime + Duration.ofSeconds(60).toMillis()), "5",},
+                        {String.valueOf(windowStartTime + Duration.ofSeconds(90).toMillis()), "20"},
+                        {String.valueOf(windowStartTime + Duration.ofSeconds(100).toMillis()),
+                            "15"},
+                        {String.valueOf(windowStartTime + Duration.ofSeconds(150).toMillis()),
+                            "60"},
+                        {String.valueOf(windowStartTime + Duration.ofSeconds(160).toMillis()),
+                            "20"},
+                        {String.valueOf(windowStartTime + Duration.ofSeconds(172).toMillis()),
+                            "30"},
+                        {String.valueOf(windowStartTime + Duration.ofSeconds(298).toMillis()),
+                            "400"}
+                    }))
+                .iterator());
+
+    Optional<NotificationEvent> notificationEventOptional =
+        alertRuleEvaluator.process(alertTaskBuilder.build());
+    Assertions.assertTrue(notificationEventOptional.isPresent());
+
+    NotificationEvent notificationEvent = notificationEventOptional.get();
+    MetricAnomalyNotificationEvent metricAnomalyNotificationEvent =
+        MetricAnomalyNotificationEvent.fromByteBuffer(
+            notificationEvent.getEventRecord().getEventValue());
+
+    assertEquals("channel1", metricAnomalyNotificationEvent.getChannelId());
+    assertEquals("event-condition-1", metricAnomalyNotificationEvent.getEventConditionId());
+    assertEquals(
+        "MetricAnomalyEventCondition", metricAnomalyNotificationEvent.getEventConditionType());
+    assertEquals(timeStamp, metricAnomalyNotificationEvent.getViolationTimestamp());
+  }
+
+  private AlertTask.Builder getTestAlertTask(
+      ViolationCondition violationCondition, long timeStamp) {
+    // create mock alertTask
+    LhsExpression lhsExpression = createLhsExpression("name", AttributeScope.SERVICE.name());
+    RhsExpression rhsExpression = createRhsExpression("customer");
+    LeafFilter leafFilter =
+        createLeafFilter(ValueOperator.VALUE_OPERATOR_EQ, lhsExpression, rhsExpression);
+
+    MetricSelection metricSelection =
+        MetricSelection.newBuilder()
+            .setMetricAggregationInterval("PT15s")
+            .setMetricAggregationFunction(
+                MetricAggregationFunction.METRIC_AGGREGATION_FUNCTION_TYPE_SUM)
+            .setFilter(Filter.newBuilder().setLeafFilter(leafFilter).build())
+            .setMetricAttribute(
+                Attribute.newBuilder().setKey("duration").setScope("SERVICE").build())
+            .build();
+
+    MetricAnomalyEventCondition.Builder metricAnomalyEventConditionBuilder =
+        MetricAnomalyEventCondition.newBuilder();
+    metricAnomalyEventConditionBuilder.setMetricSelection(metricSelection);
+    metricAnomalyEventConditionBuilder.addViolationCondition(violationCondition);
+
+    AlertTask.Builder alertTaskBuilder = AlertTask.newBuilder();
+    alertTaskBuilder.setCurrentExecutionTime(timeStamp);
+    alertTaskBuilder.setLastExecutionTime(timeStamp - Duration.ofMinutes(1).toMillis());
+    alertTaskBuilder.setEventConditionId("event-condition-1");
+    alertTaskBuilder.setEventConditionType("MetricAnomalyEventCondition");
+    alertTaskBuilder.setTenantId("__default");
+    alertTaskBuilder.setEventConditionValue(
+        metricAnomalyEventConditionBuilder.build().toByteString().asReadOnlyByteBuffer());
+    alertTaskBuilder.setChannelId("channel1");
+
+    return alertTaskBuilder;
+  }
+
+  private QueryRequest getExpectedQuery(long startTime, long endTime) {
+    LhsExpression lhsExpression = createLhsExpression("name", AttributeScope.SERVICE.name());
+    RhsExpression rhsExpression = createRhsExpression("customer");
+    LeafFilter leafFilter =
+        createLeafFilter(ValueOperator.VALUE_OPERATOR_EQ, lhsExpression, rhsExpression);
     // create mock queryServiceFilter
     org.hypertrace.core.query.service.api.Filter queryServiceFilter =
         org.hypertrace.core.query.service.api.Filter.newBuilder()
@@ -152,7 +286,7 @@ public class AlertRuleEvaluatorTest {
                                 .add("startTime")
                                 .toString(),
                             Operator.GE,
-                            alertTaskBuilder.getLastExecutionTime()))
+                            startTime))
                     .addChildFilter(
                         MetricQueryBuilder.createLongFilter(
                             new StringJoiner(".")
@@ -160,12 +294,12 @@ public class AlertRuleEvaluatorTest {
                                 .add("startTime")
                                 .toString(),
                             Operator.LT,
-                            alertTaskBuilder.getCurrentExecutionTime())))
+                            endTime)))
             .addChildFilter(MetricQueryBuilder.convertLeafFilter(leafFilter))
             .build();
 
     // create mock queryRequest
-    QueryRequest expectedQueryRequest =
+    return
         QueryRequest.newBuilder()
             .addSelection(
                 Expression.newBuilder()
@@ -193,48 +327,6 @@ public class AlertRuleEvaluatorTest {
                     MetricQueryBuilder.isoDurationToSeconds("PT15s")))
             .setLimit(QueryServiceClient.DEFAULT_QUERY_SERVICE_GROUP_BY_LIMIT)
             .build();
-
-    // create mock queryServiceClient
-    QueryServiceClient queryServiceClient = Mockito.mock(QueryServiceClient.class);
-    when(queryServiceClient.executeQuery(
-            eq(expectedQueryRequest), eq(Map.of("x-tenant-id", "__default")), eq(10000)))
-        .thenReturn(
-            List.of(
-                    getResultSetChunk(
-                        List.of(
-                            new StringJoiner(".")
-                                .add(AttributeScope.SERVICE.name())
-                                .add("startTime")
-                                .toString(),
-                            new StringJoiner(".")
-                                .add(AttributeScope.SERVICE.name())
-                                .add("duration")
-                                .toString()),
-                        new String[][] {
-                          {
-                            "60", "300",
-                          },
-                          {"120", "400"}
-                        }))
-                .iterator());
-
-    AlertRuleEvaluator alertRuleEvaluator =
-        new AlertRuleEvaluator(config, attributesServiceClient, queryServiceClient);
-
-    Optional<NotificationEvent> notificationEventOptional =
-        alertRuleEvaluator.process(alertTaskBuilder.build());
-    Assertions.assertTrue(notificationEventOptional.isPresent());
-
-    NotificationEvent notificationEvent = notificationEventOptional.get();
-    MetricAnomalyNotificationEvent metricAnomalyNotificationEvent =
-        MetricAnomalyNotificationEvent.fromByteBuffer(
-            notificationEvent.getEventRecord().getEventValue());
-
-    assertEquals("channel1", metricAnomalyNotificationEvent.getChannelId());
-    assertEquals("event-condition-1", metricAnomalyNotificationEvent.getEventConditionId());
-    assertEquals(
-        "MetricAnomalyEventCondition", metricAnomalyNotificationEvent.getEventConditionType());
-    assertEquals(timeStamp, metricAnomalyNotificationEvent.getViolationTimestamp());
   }
 
   public static ResultSetChunk getResultSetChunk(
