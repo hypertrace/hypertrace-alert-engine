8c0387c61dcd0c409fd47e9e8d4a3ba301833eeb
Sarthak Singhal <sarthaksinghal@Sarthaks-MacBook-Pro.local>
2021-08-30 12:16:33 +0530

    custom metric for alert processing time

--- hypertrace-alert-engine/src/main/java/org/hypertrace/alert/engine/RuleEvaluationJob.java
+++ hypertrace-alert-engine/src/main/java/org/hypertrace/alert/engine/RuleEvaluationJob.java
@@ -2,13 +2,16 @@ package org.hypertrace.alert.engine;
 
 import com.typesafe.config.Config;
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Timer;
 import java.io.IOException;
+import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 import org.hypertrace.alert.engine.metric.anomaly.datamodel.AlertTask;
 import org.hypertrace.alert.engine.metric.anomaly.datamodel.NotificationEvent;
 import org.hypertrace.alert.engine.metric.anomaly.detector.evaluator.AlertRuleEvaluator;
@@ -27,7 +30,12 @@ public class RuleEvaluationJob implements Job {
   private static final Logger LOGGER = LoggerFactory.getLogger(RuleEvaluationJob.class);
   private static final ConcurrentMap<String, Counter> alertProcessingErrorCounter =
       new ConcurrentHashMap<>();
-  private static final String ALERT_PROCESS_ERROR_COUNTER = "hypertrace.alert.engine.alert.task.processing.error";
+  private static final String ALERT_PROCESS_ERROR_COUNTER =
+      "hypertrace.alert.engine.alert.task.processing.error";
+  private static final ConcurrentMap<String, Timer> alertProcessingTimer =
+      new ConcurrentHashMap<>();
+  private static final String ALERT_PROCESSING_TIMER =
+      "hypertrace.alert.engine.alert.processing.time";
 
   public void execute(JobExecutionContext jobExecutionContext) {
     JobDetail jobDetail = jobExecutionContext.getJobDetail();
@@ -46,13 +54,17 @@ public class RuleEvaluationJob implements Job {
     Config jobConfig = (Config) jobDataMap.get(AlertTaskJobConstants.JOB_DATA_MAP_JOB_CONFIG);
 
     for (AlertTask.Builder alertTaskBuilder : alertTasks) {
+      Instant startTime = Instant.now();
       Instant now = AlertTaskConverter.getCurrent(jobConfig);
+
       AlertTaskConverter.setCurrentExecutionTime(alertTaskBuilder, now);
       AlertTaskConverter.setLastExecutionTime(alertTaskBuilder, now, jobConfig);
+
       LOGGER.debug(
           "Current and last time {} {}",
           Instant.ofEpochMilli(alertTaskBuilder.getCurrentExecutionTime()),
           Instant.ofEpochMilli(alertTaskBuilder.getLastExecutionTime()));
+
       try {
         Optional<NotificationEvent> notificationEventOptional =
             alertRuleEvaluator.process(alertTaskBuilder.build());
@@ -67,6 +79,14 @@ public class RuleEvaluationJob implements Job {
             .increment();
         LOGGER.error("Exception processing alertTask {}", alertTaskBuilder, e);
       }
+
+      alertProcessingTimer
+          .computeIfAbsent(
+              alertTaskBuilder.getTenantId(),
+              k ->
+                  PlatformMetricsRegistry.registerTimer(
+                      ALERT_PROCESSING_TIMER, Map.of("tenantId", k)))
+          .record(Duration.between(startTime, Instant.now()).toMillis(), TimeUnit.MILLISECONDS);
     }
   }
 }
