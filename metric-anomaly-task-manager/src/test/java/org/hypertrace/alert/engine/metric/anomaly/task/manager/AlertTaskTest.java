cbc19efd31414a9cdb1b2d3d07804696b5341338
Sarthak Singhal <sarthaksinghal@Sarthaks-MacBook-Pro.local>
2021-09-08 09:53:25 +0530

    added test for invalid rules

--- metric-anomaly-task-manager/src/test/java/org/hypertrace/alert/engine/metric/anomaly/task/manager/AlertTaskTest.java
+++ metric-anomaly-task-manager/src/test/java/org/hypertrace/alert/engine/metric/anomaly/task/manager/AlertTaskTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 import org.hypertrace.alert.engine.eventcondition.config.service.v1.Attribute;
 import org.hypertrace.alert.engine.eventcondition.config.service.v1.BaselineThresholdCondition;
 import org.hypertrace.alert.engine.eventcondition.config.service.v1.Filter;
@@ -31,8 +32,37 @@ import org.hypertrace.alert.engine.metric.anomaly.task.manager.job.AlertTaskConv
 import org.hypertrace.core.documentstore.Document;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class AlertTaskTest {
+
+  @ParameterizedTest
+  @MethodSource("provideInvalidRules")
+  void testInvalidAlertTask(String ruleId) throws Exception {
+    URL url =
+        Thread.currentThread().getContextClassLoader().getResource("./invalid-rules/" + ruleId);
+    File file = Paths.get(url.toURI()).toFile();
+    String absolutePath = file.getAbsolutePath();
+
+    Config ruleSourceConfig = ConfigFactory.parseMap(Map.of("type", "fs", "fs.path", absolutePath));
+    RuleSource ruleSource = RuleSourceProvider.getProvider(ruleSourceConfig);
+    Predicate<JsonNode> PREDICATE =
+        node -> (node.get("eventConditionType").textValue().equals(METRIC_ANOMALY_EVENT_CONDITION));
+    List<Document> documents = ruleSource.getAllRules(PREDICATE);
+    Assertions.assertTrue(documents.size() > 0);
+
+    Config jobConfig =
+        ConfigFactory.parseMap(
+            Map.of(
+                "delayInMinutes", "1",
+                "executionWindowInMinutes", 1,
+                "tenant_id", "__default"));
+    Optional<AlertTask.Builder> alertTaskBuilder =
+        new AlertTaskConverter(jobConfig).toAlertTaskBuilder(documents.get(0));
+    Assertions.assertTrue(alertTaskBuilder.isEmpty());
+  }
 
   @Test
   void testValidAlertTask() throws Exception {
@@ -60,6 +90,13 @@ class AlertTaskTest {
     MetricAnomalyEventCondition actual =
         MetricAnomalyEventCondition.parseFrom(alertTaskBuilder.get().getEventConditionValue());
     assertEquals(prepareMetricAnomalyEventCondition(), actual);
+  }
+
+  private static Stream<Arguments> provideInvalidRules() {
+    return Stream.of(
+        Arguments.arguments("invalid_alert_rule1.json"),
+        Arguments.arguments("invalid_alert_rule2.json"),
+        Arguments.arguments("invalid_alert_rule3.json"));
   }
 
   private MetricAnomalyEventCondition prepareMetricAnomalyEventCondition() {
